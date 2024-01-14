48a2a07d3000488a2eed3c179bced2e92fef63c3
Yamen <Yamen>
2024-01-13 21:04:01 -0500

    herokuV1

--- server/server.js
+++ server/server.js
@@ -343,9 +343,9 @@ function authenticateToken(req, res, next){
 }
 
 if(process.env.NODE_ENV){
-    app.use(express.static("client/build"));
-    app.get("*", (req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client", "build", "index.html"));
+    app.use(express.static(path.join(__dirname,"../client/build")));
+    app.get("/*", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../client/build/index.html"));
     });
 }
 
